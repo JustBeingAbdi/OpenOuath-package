e33c3afb54174b6d37d149d325196dd7076b1956
JustBeingAbdi <lg.tv.her.er.jeg@yandex.com>
2021-03-26 16:28:16 +0000

    +

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ exports.GenerateOuathURL = async function(callback, type) {
     if(type === 'github'){
         axios.default({
             method: 'get',
-            url: 'api.openauth.cf/github/generate/url?callback=' + callback
+            url: 'https://api.openauth.cf/github/generate/url?callback=' + callback
 
         }).then(async(response) => {
             return response.data.url;
@@ -18,7 +18,7 @@ exports.GenerateOuathURL = async function(callback, type) {
 exports.GetGithubUserInfo = async function(access_token) {
     axios.default({
         method: 'get',
-        url: 'api.openauth.cf/get/user?access_token=' + access_token,
+        url: 'https://api.openauth.cf/get/user?access_token=' + access_token,
 
     }).then(async(response) => {
         return response.data;
