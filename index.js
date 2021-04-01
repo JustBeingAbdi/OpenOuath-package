35b8f43c9937b04a2d96201afde0a684f7ced5cd
JustBeingAbdi <lg.tv.her.er.jeg@yandex.com>
2021-04-01 12:27:05 +0000

    - Removed GithubUserInfo Replaced With GetUserInfo with a "type" + Changed the Api Domain from api.openauth.cf to api.openouath.cf + Added Facebook and Google Functions
    View the Commit to read more about the changes

--- index.js
+++ index.js
@@ -7,20 +7,62 @@ exports.GenerateOuathURL = async function(callback, type) {
     if(type === 'github'){
         axios.default({
             method: 'get',
-            url: 'https://api.openauth.cf/github/generate/url?callback=' + callback
+            url: 'https://api.openouath.cf/github/generate/url?callback=' + callback
 
+        }).then(async(response) => {
+            return response.data.url;
+        });
+    }
+
+    if(type === 'google'){
+        axios.default({
+            method: 'get',
+            url: 'https://api.openouath.cf/google/generate/url?callback=' + callback
+        }).then(async(response) => {
+            return response.data.url;
+        });
+    }
+
+    if(type === 'facebook'){
+        axios.default({
+            method: 'get',
+            url: `https://api.openouath.cf/facebook/generate/url?callback=${callback}`
         }).then(async(response) => {
             return response.data.url;
         });
     }
 }
 
-exports.GetGithubUserInfo = async function(access_token) {
-    axios.default({
+exports.GetUserInfo = async function(access_token, type) {
+    
+
+    if(type === 'github'){
+        axios.default({
         method: 'get',
-        url: 'https://api.openauth.cf/get/user?access_token=' + access_token,
+        url: 'https://api.openouath.cf/github/get/user?access_token=' + access_token,
 
     }).then(async(response) => {
-        return response.data;
-    })
+            return response.data;
+        });
+    }
+
+    if(type === 'google'){
+        axios.default({
+        method: 'get',
+        url: 'https://api.openouath.cf/google/get/user?access_token=' + access_token,
+
+    }).then(async(response) => {
+            return response.data;
+        });
+    }
+
+    if(type === 'facebook'){
+        axios.default({
+        method: 'get',
+        url: 'https://api.openouath.cf/facebook/get/user?access_token=' + access_token,
+
+    }).then(async(response) => {
+            return response.data;
+        });
+    }
 }
